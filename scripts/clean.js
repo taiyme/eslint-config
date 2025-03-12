498bc5da723a2e124f19a33cfdfa18da3115aa4a
taiyme <53635909+taiyme@users.noreply.github.com>
2025-03-12 22:15:34 +0900

    update deps
    * remove rimraf
    
    * update node
    
    * update deps

---
+++ scripts/clean.js
@@ -0,0 +1,12 @@
+import fs from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+async function main() {
+  const cwd = fileURLToPath(new URL('..', import.meta.url));
+
+  await fs.rm(resolve(cwd, 'dist'), { recursive: true, force: true });
+  await fs.rm(resolve(cwd, 'node_modules'), { recursive: true, force: true });
+}
+
+await main();
---
+++ scripts/prebuild.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+async function main() {
+  const cwd = fileURLToPath(new URL('..', import.meta.url));
+
+  await fs.rm(resolve(cwd, 'dist'), { recursive: true, force: true });
+}
+
+await main();
