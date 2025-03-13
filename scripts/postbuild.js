c8ebc8b4d9e7a5a8214142d8dd24c42920abf405
taiyme <53635909+taiyme@users.noreply.github.com>
2025-03-13 20:26:43 +0900

    chore: 不要なd.tsファイルを削除する

---
+++ scripts/postbuild.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+import fs from 'node:fs/promises';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+async function main() {
+  const cwd = fileURLToPath(new URL('..', import.meta.url));
+
+  const files = await fs.readdir(resolve(cwd, 'dist'), { recursive: true });
+  const filteredFiles = files.filter((path) => path.endsWith('.d.ts') && path !== 'index.d.ts');
+
+  await Promise.all(
+    filteredFiles.map((path) => fs.unlink(resolve(cwd, 'dist', path))),
+  );
+}
+
+await main();
