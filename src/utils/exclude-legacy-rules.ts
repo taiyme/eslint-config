bd9ed96378ce4538a978fba9128c83c9a28b3c1b
taiyme <53635909+taiyme@users.noreply.github.com>
2025-04-15 14:48:26 +0900

    chore: use better-typescript-lib

--- src/utils/exclude-legacy-rules.ts
+++ src/utils/exclude-legacy-rules.ts
@@ -3,7 +3,7 @@ import type { Linter } from 'eslint';
 
 const LEGACY_RULE_LIST = Object.keys(stylisticPlugin.configs['disable-legacy'].rules ?? {});
 
-export function excludeLegacyRules(rules: Linter.RulesRecord): Linter.RulesRecord {
+export function excludeLegacyRules(rules: Partial<Linter.RulesRecord>): Partial<Linter.RulesRecord> {
   return Object.fromEntries(
     Object.entries(rules)
       .filter(([ruleName]) => !LEGACY_RULE_LIST.includes(ruleName)),
