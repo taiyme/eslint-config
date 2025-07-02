9d4503f0d833b9cd80d54e3a099bfe620906229f
taiyme <53635909+taiyme@users.noreply.github.com>
2025-07-02 18:21:39 +0900

    refactor: postbuildで保持するファイルを定数化

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,11 +2,15 @@ import fs from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+const KEEP_DTS_FILES = [
+  'index.d.ts',
+];
+
 async function main() {
   const cwd = fileURLToPath(new URL('..', import.meta.url));
 
   const files = await fs.readdir(resolve(cwd, 'dist'), { recursive: true });
-  const filteredFiles = files.filter((path) => path.endsWith('.d.ts') && path !== 'index.d.ts');
+  const filteredFiles = files.filter((path) => path.endsWith('.d.ts') && !KEEP_DTS_FILES.includes(path));
 
   await Promise.all(
     filteredFiles.map((path) => fs.unlink(resolve(cwd, 'dist', path))),
