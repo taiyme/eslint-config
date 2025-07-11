085871ca52613c3052a51a8656b6543ee13def87
taiyme <53635909+taiyme@users.noreply.github.com>
2025-07-11 16:05:23 +0900

    chore(build): migrate to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: 'src/index.ts',
+  outDir: 'dist',
+  format: 'esm',
+  platform: 'node',
+  dts: true,
+  minify: true,
+  exports: false,
+});
